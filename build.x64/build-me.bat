5fdba903a0f137d68e127747d394edaece7fd218
Geoff McLane <ubuntu@geoffair.info>
2017-12-24 19:11:25 +0100

    Add WIN64 build folder

---
+++ build.x64/build-me.bat
@@ -0,0 +1,130 @@
+@setlocal
+@set TMPPRJ=FA4
+@set TMPLOG=bldlog-1.txt
+@set TMPSRC=..
+@set VCVERS=14
+@REM ############################################
+@REM NOTE: SPECIAL INSTALL LOCATION
+@REM Adjust to suit your environment
+@REM ##########################################
+@set TMPINST=C:\MDOS
+@set TMPOPTS=-DCMAKE_INSTALL_PREFIX=%TMPINST%
+
+@set GENERATOR=Visual Studio %VCVERS% Win64
+@set VS_PATH=C:\Program Files (x86)\Microsoft Visual Studio %VCVERS%.0
+@set VC_BAT=%VS_PATH%\VC\vcvarsall.bat
+@if NOT EXIST "%VS_PATH%" goto NOVS
+@if NOT EXIST "%VC_BAT%" goto NOBAT
+@set BUILD_BITS=%PROCESSOR_ARCHITECTURE%
+
+@set TMPOPTS=%TMPOPTS% -G "%GENERATOR%"
+
+@call chkmsvc %TMPPRJ%
+
+@echo Begin %DATE% %TIME%, output to %TMPLOG%
+@echo Begin %DATE% %TIME% > %TMPLOG%
+
+@echo Setting environment - CALL "%VC_BAT%" %BUILD_BITS%
+@echo Setting environment - CALL "%VC_BAT%" %BUILD_BITS% >> %TMPLOG%
+@call "%VC_BAT%" %BUILD_BITS% >> %TMPLOG% 2>&1
+@if ERRORLEVEL 1 goto NOSETUP
+
+@echo Doing: 'cmake %TMPSRC% %TMPOPTS%'
+@echo Doing: 'cmake %TMPSRC% %TMPOPTS%' >> %TMPLOG%
+@cmake %TMPSRC% %TMPOPTS% >> %TMPLOG% 2>&1
+@if ERRORLEVEL 1 goto ERR1
+
+@echo Doing: 'cmake --build . --config debug'
+@echo Doing: 'cmake --build . --config debug' >> %TMPLOG%
+@cmake --build . --config debug >> %TMPLOG%
+@if ERRORLEVEL 1 goto ERR2
+
+@echo Doing: 'cmake --build . --config release'
+@echo Doing: 'cmake --build . --config release' >> %TMPLOG%
+@cmake --build . --config release >> %TMPLOG% 2>&1
+@if ERRORLEVEL 1 goto ERR3
+
+@echo Appears a successful build
+@echo.
+@REM echo No install at this time...
+@REM goto END
+@if NOT EXIST Release\fa4.exe goto DNCHK
+@if NOT EXIST C:\MDOS\fa4.exe goto DNCHK
+@REM fc4 exit Errorlevel 1 if Different; 2 if ERROR and 3 if HELP shown, else 0.
+@fc4 -? >nul
+@if ERRORLEVEL 4 goto DNCHK
+@if ERRORLEVEL 3 goto DOCHK
+@goto DNCHK
+:DOCHK
+@fc4 -v0 -q -b Release\fa4.exe C:\MDOS\fa4.exe
+@if ERRORLEVEL 1 goto DNCHK
+@echo Installed C:\MDOS\fa4.exe EXACTLY SAME as Release\fa4.exe
+@echo NO INSTALL REQUIRED
+@echo.
+@goto END
+
+:DNCHK
+@echo.
+@echo Note install location %TMPINST%
+@echo *** CONTINUE with install? *** Only 'y' continues ***
+@echo.
+@ask
+@if ERRORLEVEL 2 goto NOASK
+@if ERRORLEVEL 1 goto DOINST
+@echo.
+@echo No install at this time...
+@echo.
+@goto END
+
+:DOINST
+cmake -P cmake_install.cmake
+@REM echo Doing: 'cmake --build . --config release --target INSTALL'
+@REM echo Doing: 'cmake --build . --config release --target INSTALL' >> %TMPLOG%
+@REM cmake --build . --config release --target INSTALL >> %TMPLOG% 2>&1
+@REM fa4 " -- " %TMPLOG%
+
+@echo Done build and install of %TMPPRJ%...
+
+@goto END
+
+:NOASK
+@echo.
+@echo Can NOT find 'ask' utility in PATH
+@echo See : https://gitorious.org/fgtools/gtools
+@echo.
+@goto END
+
+
+:ERR1
+@echo cmake config, generation error
+@goto ISERR
+
+:ERR2
+@echo debug build error
+@goto ISERR
+
+:ERR3
+@echo release build error
+@goto ISERR
+
+:NOVS
+@echo Can not locate "%VS_PATH%"! *** FIX ME *** for your environment
+@goto ISERR
+
+:NOBAT
+@echo Can not locate "%VC_BAT%"! *** FIX ME *** for your environment
+@goto ISERR
+
+:NOSETUP
+@echo MSVC setup FAILED!
+@goto ISERR
+
+:ISERR
+@endlocal
+@exit /b 1
+
+:END
+@endlocal
+@exit /b 0
+
+@REM eof
